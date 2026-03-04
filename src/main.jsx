f25910fac3d80ad80f3d00d59722f3f3ea957105
clement <clement2bamogo@gmail.com>
2026-03-04 02:11:23 +0100

    mmm

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import App from './App.jsx'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  // <HashRouter>
-  //   <App />
-  // </HashRouter>,
-  <BrowserRouter>
-    <App></App>
-  </BrowserRouter>
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  // <BrowserRouter>
+  //   <App></App>
+  // </BrowserRouter>
 )
